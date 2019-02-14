17b919de908480e91c27d6a2582438f43892773f
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-14 09:41:41 +0300

    StartCommit

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,16 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else b %= a;
+            }
+
+            return a + b;
         }
     }
 }
